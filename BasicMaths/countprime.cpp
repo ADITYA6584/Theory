984e4307a50bb30cc923b95bf856062a07584826
ADITYA6584 <akskdk678@gmail.com>
2024-08-29 21:57:37 +0530

    Basic Maths Done

--- BasicMaths/countprime.cpp
+++ BasicMaths/countprime.cpp
@@ -4,29 +4,31 @@
 #include <vector>
 using namespace std;
 
-//Approach -> 2 Best approach 
+//Approach -> 2 Best approach ***********************************************
 
 //This is sieve of eratosthenes  
-/*
-Time Complexity is : O(nlog(log(n)))
-*/
-class Solution {
-public:
-    int countPrimes(int n) {
-        int cnt = 0;
-        vector <bool> prime(n + 1, true);
-        prime[0] = prime[1] = false;
-        for (int i = 2; i < n; i++) {
-            if (prime[i]) {
-                cnt++;
-                for (int j = i * 2; j < n; j = j + i) {
-                    prime[j] = 0;
-                }
-            }
-        }
-        return cnt;
-    }
-};
+
+
+// Time Complexiy = O(nlog(log(n)))
+
+
+// class Solution {
+// public:
+//     int countPrimes(int n) {
+//         int cnt = 0;
+//         vector <bool> prime(n + 1, true);
+//         prime[0] = prime[1] = false;
+//         for (int i = 2; i < n; i++) {
+//             if (prime[i]) {
+//                 cnt++;
+//                 for (int j = i * 2; j < n; j = j + i) {
+//                     prime[j] = 0;
+//                 }
+//             }
+//         }
+//         return cnt;
+//     }
+// };
 
 //Approach -> 1 but TLE ho rha hai
 class Solution {
