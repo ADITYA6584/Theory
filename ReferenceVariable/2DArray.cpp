29a3273fc87467670b47bb228a37eff99fde2bb5
ADITYA6584 <akskdk678@gmail.com>
2024-09-05 01:09:37 +0530

    add

---
+++ ReferenceVariable/2DArray.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+//Creating 2D Array here using dynamic memory allocation
+
+int main()
+{
+    int r,c;
+    cin>>r>>c;
+    int *ptr1 = new int[r];
+    int *ptr2 = new int[c];
+    for(int i =0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            
+        }
+    }
+
+    return 0;
+}
